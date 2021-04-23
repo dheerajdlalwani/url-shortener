c357f83c2c50a5c5a5f1c935fc126786f6f884cf
dheerajdlalwani <lalwanidheeraj1234@gmail.com>
2021-04-23 14:46:12 +0530

    ## Task 2 Fixes #2

--- core/main.py
+++ core/main.py
@@ -1,11 +1,31 @@
 from flask import Flask, render_template, request, redirect
 
+from models import *
+
 project = Flask(__name__)
 
 
-@project.route('/')
+@project.route('/', methods=['POST', 'GET'])
 def home():
-    return render_template('home.html')
+    if request.method == "POST":
+        data = request.form
+        long_url = data['long_url']
+        short_url = 'dheeraj-twitter
+        return render_template('home.html', short_url=short_url)
+    else:
+        return render_template('home.html')
+
+
+
+
+
+@project.route('/<string:short_url>', methods=['GET'])
+def transport(short_url):
+    print(short_url)
+    if short_url == "dheeraj-twitter":
+        return redirect('https://twitter.com/dhirucodes')
+    else:
+        pass
 
 
 if __name__ == "__main__":
