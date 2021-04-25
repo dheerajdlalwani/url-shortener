cf02ea8e47962ea4edd2cbe014b3c4129636404f
dheerajdlalwani <lalwanidheeraj1234@gmail.com>
2021-04-25 11:57:18 +0530

    hosting attempt 8

--- core/main.py
+++ core/main.py
@@ -1,7 +1,27 @@
 from flask import Flask, render_template, request, redirect
 import random
 import string
-from models import *
+# from models import *
+import os
+import pymongo
+from pymongo import MongoClient
+from dotenv import load_dotenv
+load_dotenv()
+
+
+MONGO_CLIENT = os.getenv('CLIENT')
+cluster = MongoClient(MONGO_CLIENT)
+db = cluster["URL-Shortener"]
+collection = db["url"]
+
+new_url = {
+    "long_url": "",
+    "short_url": "",
+    "click_count": 0
+}
+
+
+
 
 project = Flask(__name__)
 
